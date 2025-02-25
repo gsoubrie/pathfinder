c38da0a77ef4c6d237ef35dd9f533bf4386bba24
GSOU <g.soubrie@shinken-solutions.com>
2025-02-25 08:35:37 +0100

    adding libs

--- Pathfinder/Feuille_de_perso/js/popup.js
+++ Pathfinder/Feuille_de_perso/js/popup.js
@@ -7,26 +7,32 @@ POPUP.PropertyEdition.prototype = {
     init: function () {
         this.windows = new WINDOW_V2.ElementGroupFromData( "tabs" );
         this.windows.initWithData( this.current_property.ENUM );
-        this.windows.setActiveWindow(  );
+        this.windows.setActiveWindow( "elfe" );
         
         console.log( "GSOU", "[PropertyEdition - init]", this.windows );
         this.computeHtml();
     },
     //********************************************  EVENT LISTENER  **************************************************//
-    changeTabPopup: function ( tab_name ) {
-        this.tabs.changeTabPopup( tab_name );
-    },
+    
     //********************************************  SAVE   **************************************************//
     
     //********************************************  HTML   **************************************************//
     computeHtml: function () {
         this.setDomElement( SERVICE.DOM.createElement( "div", { class: "popup" } ) );
-        let container = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "edition-property-popup" } ), this.getDomElement() );
-        SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "title" }, this.current_property.label ), container );
+        this.container = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "edition-property-popup" } ), this.getDomElement() );
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "title" }, this.current_property.label ), this.container );
         this.windows.computeHtml();
-        SERVICE.DOM.addElementTo( this.windows.getDomElement(), container );
+        SERVICE.DOM.addElementTo( this.windows.getDomElement(), this.container );
         
         SERVICE.DOM.addElementTo( this.getDomElement(), document.querySelector( "body" ) );
+        
+        for ( var i = 0, _size_i = this.windows.getSize(); i < _size_i; i++ ) {
+            this.windows.getContent(i).setContentDomElementTarget();
+        }
+        for ( let i = 0, _size_i = _list.length; i < _size_i; i++ ) {
+        
+        }
+        this.windows.computeHtmlTargets( this.container );
     }
 };
 
