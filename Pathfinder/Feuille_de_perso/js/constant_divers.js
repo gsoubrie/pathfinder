7c7de5ffb4b43ebb77cebe9c1f292b9e7e623476
GSOU <g.soubrie@shinken-solutions.com>
2025-04-09 14:02:40 +0200

    ending script historic

--- Pathfinder/Feuille_de_perso/js/constant_divers.js
+++ Pathfinder/Feuille_de_perso/js/constant_divers.js
@@ -55,3 +55,108 @@ var VM             = {
     _1: "faible",
     _2: "sombre"
 };
+var SKILLS         = {
+    LIST            : {
+        ACROBATICS  : {
+            label         : "Acrobaties",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        },
+        ARCANA      : {
+            label         : "Arcanes",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        CRAFTING    : {
+            label         : "Artisanat",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key,
+            need_qualifier: true
+        },
+        ATHLETICS   : {
+            label         : "Athlétisme",
+            characteristic: CHARACTERISTIC.FORCE.key,
+            armor_penalty : true
+        },
+        LORE        : {
+            label         : "Connaissance",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key,
+            need_qualifier: true
+        },
+        DIPLOMACY   : {
+            label         : "Diplomatie",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        STEALTH     : {
+            label         : "Discrétion",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        },
+        DECEPTION   : {
+            label         : "Duperie",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        INTIMIDATION: {
+            label         : "Intimidation",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        MEDICINE    : {
+            label         : "Médécine",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        NATURE      : {
+            label         : "Nature",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        OCCULTISM   : {
+            label         : "Occultisme",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        RELIGION    : {
+            label         : "Religion",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        PERFORMANCE : {
+            label         : "Représentation",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        SOCIETY     : {
+            label         : "Société",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        SURVIVAL    : {
+            label         : "Survie",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        THIEVERY    : {
+            label         : "Vol",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        }
+    },
+    parseTextInSkill: function ( text ) {
+        let to_return = [];
+        let split     = text.split( " " );
+        for ( let i = 0, _size_i = split.length; i < _size_i; i++ ) {
+            let is_as_skill = SKILLS.isASkill( split[ i ] );
+            if ( is_as_skill ) {
+                to_return.push( is_as_skill );
+            }
+            if ( is_as_skill.need_qualifier ) {
+                let split_qualifier   = text.split( is_as_skill.label )[ 1 ];
+                split_qualifier       = split_qualifier.split( "." )[ 0 ];
+                is_as_skill.qualifier = split_qualifier;
+            }
+        }
+        return to_return;
+    },
+    isASkill        : function ( word ) {
+        var _keys = Object.keys( SKILLS.LIST );
+        var _current;
+        for ( let i = 0, _size = _keys.length; i < _size; i++ ) {
+            _current = SKILLS.LIST[ _keys[ i ] ];
+            if ( _current.label.toLowerCase() === word.toLowerCase() ) {
+                return JSON.parse(JSON.stringify(_current));
+            }
+        }
+        return false;
+    }
+};
--- Pathfinder/Feuille_de_perso/js/script/script_historique.js
+++ Pathfinder/Feuille_de_perso/js/script/script_historique.js
@@ -82,17 +82,12 @@ var SCRIPT_HISTORIQUES = (function ( self ) {
     self.parseTextToSkill    = function ( text, rank_skill, object_to_complete ) {
         object_to_complete[ "skills" ]               = object_to_complete[ "skills" ] || {};
         object_to_complete[ "skills" ][ rank_skill ] = object_to_complete[ "skills" ][ rank_skill ] || [];
-        let split                                    = text.split( "en" );
-        for ( let i = 1, _size_i = split.length; i < _size_i; i++ ) {
-            object_to_complete[ "skills" ][ rank_skill ].push( self.cleanTextToSkill( split[ i ] ) );
+        let skills                                   = SKILLS.parseTextInSkill( text );
+        for ( let i = 0, _size_i = skills.length; i < _size_i; i++ ) {
+            object_to_complete[ "skills" ][ rank_skill ].push( skills[ i ] );
         }
+        
     };
-    self.cleanTextToSkill    = function ( text_skill ) {
-        text_skill = SERVICE.STRING.replaceAll( text_skill, "et", "" );
-        text_skill = text_skill.trim();
-        return text_skill;
-    };
-    
     return self;
 })( SCRIPT_HISTORIQUES || {} );
 
@@ -172,6 +167,111 @@ var CHARACTERISTIC = {
     FREE        : {
         key  : "FRE",
         label: "Libre"
+    }
+};
+var SKILLS         = {
+    LIST            : {
+        ACROBATICS  : {
+            label         : "Acrobaties",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        },
+        ARCANA      : {
+            label         : "Arcanes",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        CRAFTING    : {
+            label         : "Artisanat",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key,
+            need_qualifier: true
+        },
+        ATHLETICS   : {
+            label         : "Athlétisme",
+            characteristic: CHARACTERISTIC.FORCE.key,
+            armor_penalty : true
+        },
+        LORE        : {
+            label         : "Connaissance",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key,
+            need_qualifier: true
+        },
+        DIPLOMACY   : {
+            label         : "Diplomatie",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        STEALTH     : {
+            label         : "Discrétion",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        },
+        DECEPTION   : {
+            label         : "Duperie",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        INTIMIDATION: {
+            label         : "Intimidation",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        MEDICINE    : {
+            label         : "Médécine",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        NATURE      : {
+            label         : "Nature",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        OCCULTISM   : {
+            label         : "Occultisme",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        RELIGION    : {
+            label         : "Religion",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        PERFORMANCE : {
+            label         : "Représentation",
+            characteristic: CHARACTERISTIC.CHARISMA.key
+        },
+        SOCIETY     : {
+            label         : "Société",
+            characteristic: CHARACTERISTIC.INTELLIGENCE.key
+        },
+        SURVIVAL    : {
+            label         : "Survie",
+            characteristic: CHARACTERISTIC.WISDOM.key
+        },
+        THIEVERY    : {
+            label         : "Vol",
+            characteristic: CHARACTERISTIC.DEXTERITY.key,
+            armor_penalty : true
+        }
+    },
+    parseTextInSkill: function ( text ) {
+        let to_return = [];
+        let split     = text.split( " " );
+        for ( let i = 0, _size_i = split.length; i < _size_i; i++ ) {
+            let is_as_skill = SKILLS.isASkill( split[ i ] );
+            if ( is_as_skill ) {
+                to_return.push( is_as_skill );
+            }
+            if ( is_as_skill.need_qualifier ) {
+                let split_qualifier   = text.split( is_as_skill.label )[ 1 ];
+                split_qualifier       = split_qualifier.split( "." )[ 0 ];
+                is_as_skill.qualifier = split_qualifier;
+            }
+        }
+        return to_return;
+    },
+    isASkill        : function ( word ) {
+        var _keys = Object.keys( SKILLS.LIST );
+        var _current;
+        for ( let i = 0, _size = _keys.length; i < _size; i++ ) {
+            _current = SKILLS.LIST[ _keys[ i ] ];
+            if ( _current.label.toLowerCase() === word.toLowerCase() ) {
+                return JSON.parse(JSON.stringify(_current));
+            }
+        }
+        return false;
     }
 };
 
