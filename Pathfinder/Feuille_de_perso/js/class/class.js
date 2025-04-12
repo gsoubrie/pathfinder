088a9b9365c4c0b012804646ca0ff7960c328f51
gregoire.soubrie <gregoire.soubrie@gmail.com>
2025-04-13 00:20:54 +0200

    gg

--- Pathfinder/Feuille_de_perso/js/class/class.js
+++ Pathfinder/Feuille_de_perso/js/class/class.js
@@ -8,24 +8,46 @@ CLASSES.Class.prototype = {
     },
     //********************************************  EVENT LISTENER  **************************************************//
     getUUID: function () {
-        return this.name_lower_case;
+        return this.name;
     },
     //********************************************  HTML   **************************************************//
     setData: function ( key, value ) {
         switch ( key ) {
             case "name":
-            case "name_lower_case":
             case "general_desc":
+            case "capacities":
+            case "prime_attributes":
+            case "life_point_by_level":
+            case "desc_fight":
+            case "desc_socially":
+            case "desc_exploration":
+            case "desc_interlude":
+            case "desc_you_could":
+            case "desc_probably_others":
+            case "mastery_initial":
+            case "prime_attribute":
+            case "mastery_perception":
+            case "mastery_js":
+            case "mastery_skill":
+            case "mastery_attack":
+            case "mastery_defense":
+            case "mastery_dd":
+            case "capacity_by_level":
+            case "mastery_spell":
+            case "mastery_rarity":
                 this[ key ] = value;
                 break;
             default:
-                console.warn( "[IGNORED DATA]", key, value );
+                console.warn( "[IGNORED DATA]", key );
         }
     },
     //********************************************  HTML   **************************************************//
     computeHTMLEdition: function () {
-        this.setDomElement( SERVICE.DOM.createElement( "div", { class: "race-edition" } ) );
-        SERVICE.DOM.addElementTo( SERVICE.DOM.createEditionPropertyDescription( this.general_desc, "Description" ), this.getDomElement() );
+        this.setDomElement( SERVICE.DOM.createElement( "div", { class: "class-edition" } ) );
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createEditionPropertyHorizontal( this.general_desc, "" ), this.getDomElement() );
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createEditionPropertyHorizontal( this.life_point_by_level, "Point de vie par level" ), this.getDomElement() );
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createEditionPropertyHorizontal( this.prime_attribute, "Prime d'attribut" ), this.getDomElement() );
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createEditionPropertyDescription( this.capacities, "Capacités" ), this.getDomElement() );
         return this.getDomElement();
     }
 };
