0e7f92784d662004d1d0bfe496c0d496a9774f56
GSOU <g.soubrie@shinken-solutions.com>
2025-11-03 13:58:53 +0100

    start container skill in level tab

--- Pathfinder/Feuille_de_perso/js/character/character_windows/level_window.js
+++ Pathfinder/Feuille_de_perso/js/character/character_windows/level_window.js
@@ -11,6 +11,12 @@ CHARACTER.LevelWindow.prototype = {
     computeHtmlWithData: function ( character_object ) {
         this.content_dom_element_target = SERVICE.DOM.createElement( "div", { class: "level-window" } );
         
+        this.computeHtml__life(character_object);
+        this.computeHtml__skill(character_object);
+        
+        SERVICE.DOM.addElementToAfterEmpty( this.content_dom_element_target, this.dom_element_target );
+    },
+    computeHtml__life: function ( character_object ) {
         const life_container = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "level-container-life" } ), this.content_dom_element_target );
         const title = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "level-title" }, "<hr>" ), life_container );
         
@@ -34,30 +40,29 @@ CHARACTER.LevelWindow.prototype = {
             SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "td", {}, lvl.bonus || "" ), tr );
         } );
         
-        // ---- Bonus hors niveau ----
-        //const outsideBonus = character_object.getLevelHistory().getOutsideBonus
-        //                     ? character_object.getLevelHistory().getOutsideBonus()
-        //                     : [];
-        //
-        //if ( outsideBonus.length > 0 ) {
-        //    const bonusContainer = SERVICE.DOM.addElementTo(
-        //        SERVICE.DOM.createElement( "div", { class: "bonus-outside" } ),
-        //        this.content_dom_element_target
-        //    );
-        //
-        //    outsideBonus.forEach( b => {
-        //        SERVICE.DOM.addElementTo(
-        //            SERVICE.DOM.createElement(
-        //                "div",
-        //                { class: "bonus-line" },
-        //                (b.value ? ("+" + b.value) : "") + " " + (b.label || "")
-        //            ),
-        //            bonusContainer
-        //        );
-        //    } );
-        //}
+        SERVICE.DOM.addElementToAfterEmpty( this.content_dom_element_target, this.dom_element_target );
+    },
+    computeHtml__skill: function ( character_object ) {
+        const container = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "level-container-skill" } ), this.content_dom_element_target );
+        const title = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "skill-title" }, "<hr>" ), container );
         
-        // ---- Ajout final dans la fenêtre ----
+        SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "div", { class: "" }, "Compéténce : " ), container );
+        
+        const table = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "table", { class: "skill-table" } ), container );
+        
+        const headerRow = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "tr" ), table );
+        
+        ["Niv", "Value"].forEach( label => {
+            SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "th", {}, label ), headerRow );
+        } );
+        
+        character_object.getLevelHistory().contents.forEach( lvl => {
+            const tr = SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "tr" ), table );
+            
+            SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "td", {}, lvl.getUUID() ), tr );
+            SERVICE.DOM.addElementTo( SERVICE.DOM.createElement( "td", {},  "" ), tr );
+        } );
+        
         SERVICE.DOM.addElementToAfterEmpty( this.content_dom_element_target, this.dom_element_target );
     }
 };
