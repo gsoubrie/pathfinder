e63f328cf0f9867128a0aeb86a05a00802be8e8d
GSOU <g.soubrie@shinken-solutions.com>
2026-05-25 14:37:59 +0200

    complete object informations missing

---
+++ sheet/js/object/constant_objects3.js
@@ -0,0 +1,446 @@
+OBJECT.CONST = {
+    "KxpaxUSuBC7hr4F7": {
+        "id"      : "KxpaxUSuBC7hr4F7",
+        "text"    : "Aboutissement exemplaire",
+        "category": "feat",
+        "href"    : "/feats/KxpaxUSuBC7hr4F7"
+    },
+    "LbPLNWlLCxKCo5gF": {
+        "id"      : "LbPLNWlLCxKCo5gF",
+        "text"    : "Accès au savoir",
+        "category": "feat",
+        "href"    : "/feats/LbPLNWlLCxKCo5gF"
+    },
+    "tlSE7Ly8vi1Dgddv": {
+        "id"      : "tlSE7Ly8vi1Dgddv",
+        "text"    : "Accès de rigolade",
+        "category": "feat",
+        "href"    : "/feats/tlSE7Ly8vi1Dgddv"
+    },
+    "pHrVvoTKygXeczVG": {
+        "id"      : "pHrVvoTKygXeczVG",
+        "text"    : "Amulette de la nymphe",
+        "category": "item",
+        "href"    : "/equipment/pHrVvoTKygXeczVG"
+    },
+    "KR8WgdazifDBjDkW": {
+        "id"      : "KR8WgdazifDBjDkW",
+        "text"    : "Aubaine inattendue",
+        "category": "feat",
+        "href"    : "/feats/KR8WgdazifDBjDkW"
+    },
+    "enQieRrITuEQZxx2": {
+        "id"      : "enQieRrITuEQZxx2",
+        "text"    : "Bouclier égoïste",
+        "category": "spell",
+        "href"    : "/spells/enQieRrITuEQZxx2"
+    },
+    "pIHjH1x0AVtiX5Tv": {
+        "id"      : "pIHjH1x0AVtiX5Tv",
+        "text"    : "C'est étrange",
+        "category": "spell",
+        "href"    : "/spells/pIHjH1x0AVtiX5Tv"
+    },
+    "Nun72GTmb31YqSKh": {
+        "id"      : "Nun72GTmb31YqSKh",
+        "text"    : "Cape d'invisibilité",
+        "category": "item",
+        "href"    : "/equipment/Nun72GTmb31YqSKh"
+    },
+    "AlbpWWN87yGegoAF": {
+        "id"      : "AlbpWWN87yGegoAF",
+        "text"    : "Cible verrouillée",
+        "category": "feat",
+        "href"    : "/feats/AlbpWWN87yGegoAF"
+    },
+    "jSkJIWPfSZZzvYzq": {
+        "id"      : "jSkJIWPfSZZzvYzq",
+        "text"    : "Combiner des élixirs",
+        "category": "action",
+        "href"    : "/actions/jSkJIWPfSZZzvYzq"
+    },
+    "VjQfJKeDc46cWtgk": {
+        "id"      : "VjQfJKeDc46cWtgk",
+        "text"    : "Communication zoophonique",
+        "category": "spell",
+        "href"    : "/spells/VjQfJKeDc46cWtgk"
+    },
+    "f2Pl5dWEL9ZvEyI1": {
+        "id"      : "f2Pl5dWEL9ZvEyI1",
+        "text"    : "Compagnon animal",
+        "category": "spell",
+        "href"    : "/spells/f2Pl5dWEL9ZvEyI1"
+    },
+    "sVjATEo8eqkAosNp": {
+        "id"      : "sVjATEo8eqkAosNp",
+        "text"    : "Composition prolongée",
+        "category": "action",
+        "href"    : "/actions/sVjATEo8eqkAosNp"
+    },
+    "G0T1xv1FoZ23Jxvt": {
+        "id"      : "G0T1xv1FoZ23Jxvt",
+        "text"    : "Coup de pouce du destin",
+        "category": "feat",
+        "href"    : "/feats/G0T1xv1FoZ23Jxvt"
+    },
+    "YKexU7BkwNq4ESCc": {
+        "id"      : "YKexU7BkwNq4ESCc",
+        "text"    : "Dentelé",
+        "category": "condition",
+        "href"    : "/conditions/YKexU7BkwNq4ESCc"
+    },
+    "qVNVSmsgpKFGk9hV": {
+        "id"      : "qVNVSmsgpKFGk9hV",
+        "text"    : "Dissimulé cet objet",
+        "category": "spell",
+        "href"    : "/spells/qVNVSmsgpKFGk9hV"
+    },
+    "rmnhzmJqfzEVnz1h": {
+        "id"      : "rmnhzmJqfzEVnz1h",
+        "text"    : "Drain de familier",
+        "category": "spell",
+        "href"    : "/spells/rmnhzmJqfzEVnz1h"
+    },
+    "aqnx6IDcB7ARLxS5": {
+        "id"      : "aqnx6IDcB7ARLxS5",
+        "text"    : "Effet : Magie du sang Béni du ver (pénalité de statut - cible)",
+        "category": "effect",
+        "href"    : "/effects/aqnx6IDcB7ARLxS5"
+    },
+    "ruRAfGJnik7lRavk": {
+        "id"      : "ruRAfGJnik7lRavk",
+        "text"    : "Effet : Magie du sang nymphe (cible) (Soi)",
+        "category": "effect",
+        "href"    : "/effects/ruRAfGJnik7lRavk"
+    },
+    "UpEjRfQkCJCruAfb": {
+        "id"      : "UpEjRfQkCJCruAfb",
+        "text"    : "Enfant de la tempête",
+        "category": "spell",
+        "href"    : "/spells/UpEjRfQkCJCruAfb"
+    },
+    "LyqqvufOUIh8U25d": {
+        "id"      : "LyqqvufOUIh8U25d",
+        "text"    : "Extension de magie du sang",
+        "category": "feat",
+        "href"    : "/feats/LyqqvufOUIh8U25d"
+    },
+    "acJkVtpX2GUvl90E": {
+        "id"      : "acJkVtpX2GUvl90E",
+        "text"    : "Faire pencher la balance",
+        "category": "feat",
+        "href"    : "/feats/acJkVtpX2GUvl90E"
+    },
+    "VaIHQzOE5ibmbtqU": {
+        "id"      : "VaIHQzOE5ibmbtqU",
+        "text"    : "Forme indomptée",
+        "category": "spell",
+        "href"    : "/spells/VaIHQzOE5ibmbtqU"
+    },
+    "RQH6vigvhmiYKKjg": {
+        "id"      : "RQH6vigvhmiYKKjg",
+        "text"    : "Frappe précise",
+        "category": "feat",
+        "href"    : "/feats/RQH6vigvhmiYKKjg"
+    },
+    "EAP98XaChJEbgKcK": {
+        "id"      : "EAP98XaChJEbgKcK",
+        "text"    : "Frappe punitive",
+        "category": "feat",
+        "href"    : "/feats/EAP98XaChJEbgKcK"
+    },
+    "sGenGMmE1ntkXCtN": {
+        "id"      : "sGenGMmE1ntkXCtN",
+        "text"    : "Incantation Interdisciplinaire",
+        "category": "feat",
+        "href"    : "/feats/sGenGMmE1ntkXCtN"
+    },
+    "zJxUflt9np0q4yML": {
+        "id"      : "zJxUflt9np0q4yML",
+        "text"    : "Invisible",
+        "category": "condition",
+        "href"    : "/conditions/zJxUflt9np0q4yML"
+    },
+    "au0lwQ1nAcNQwcGh": {
+        "id"      : "au0lwQ1nAcNQwcGh",
+        "text"    : "Lien arcanique",
+        "category": "spell",
+        "href"    : "/spells/au0lwQ1nAcNQwcGh"
+    },
+    "Dy1cyGJUfzyZ4EIy": {
+        "id"      : "Dy1cyGJUfzyZ4EIy",
+        "text"    : "Lignages d'ensorceleur draconique",
+        "category": "feat",
+        "href"    : "/feats/Dy1cyGJUfzyZ4EIy"
+    },
+    "tuZnRWHixLArvaIf": {
+        "id"      : "tuZnRWHixLArvaIf",
+        "text"    : "Lueur de rédemption",
+        "category": "spell",
+        "href"    : "/spells/tuZnRWHixLArvaIf"
+    },
+    "PRrZ7anETWPm90YY": {
+        "id"      : "PRrZ7anETWPm90YY",
+        "text"    : "Magie déguisée",
+        "category": "feat",
+        "href"    : "/feats/PRrZ7anETWPm90YY"
+    },
+    "kUSShxXzY1sPtJA0": {
+        "id"      : "kUSShxXzY1sPtJA0",
+        "text"    : "Magie détonante",
+        "category": "feat",
+        "href"    : "/feats/kUSShxXzY1sPtJA0"
+    },
+    "XQjR07LkDedC7tkc": {
+        "id"      : "XQjR07LkDedC7tkc",
+        "text"    : "Malédiction de l'afflux de connaissances",
+        "category": "spell",
+        "href"    : "/spells/XQjR07LkDedC7tkc"
+    },
+    "d03gBFLK4XJlDNNh": {
+        "id"      : "d03gBFLK4XJlDNNh",
+        "text"    : "Malédiction de l'appel des cieux",
+        "category": "spell",
+        "href"    : "/spells/d03gBFLK4XJlDNNh"
+    },
+    "vXnMKFIxqLuCDW9q": {
+        "id"      : "vXnMKFIxqLuCDW9q",
+        "text"    : "Malédiction de l'incursion ancestrale",
+        "category": "spell",
+        "href"    : "/spells/vXnMKFIxqLuCDW9q"
+    },
+    "8ODGE24gqEdzWljj": {
+        "id"      : "8ODGE24gqEdzWljj",
+        "text"    : "Malédiction de la mort vivante",
+        "category": "spell",
+        "href"    : "/spells/8ODGE24gqEdzWljj"
+    },
+    "5QVUaQGT1FSkFOYP": {
+        "id"      : "5QVUaQGT1FSkFOYP",
+        "text"    : "Malédiction de la pourriture inévitable",
+        "category": "spell",
+        "href"    : "/spells/5QVUaQGT1FSkFOYP"
+    },
+    "zO7dvBgLuhdGfP5t": {
+        "id"      : "zO7dvBgLuhdGfP5t",
+        "text"    : "Malédiction de la vie déversée",
+        "category": "spell",
+        "href"    : "/spells/zO7dvBgLuhdGfP5t"
+    },
+    "2VclK5CWR1Vz1vqL": {
+        "id"      : "2VclK5CWR1Vz1vqL",
+        "text"    : "Malédiction des cendres rampantes",
+        "category": "spell",
+        "href"    : "/spells/2VclK5CWR1Vz1vqL"
+    },
+    "RI2UHuUZd3TC1OE8": {
+        "id"      : "RI2UHuUZd3TC1OE8",
+        "text"    : "Malédiction des flammes engloutissantes",
+        "category": "spell",
+        "href"    : "/spells/RI2UHuUZd3TC1OE8"
+    },
+    "X6GQ4ngqpjP8SpCq": {
+        "id"      : "X6GQ4ngqpjP8SpCq",
+        "text"    : "Malédiction des moments de turbulence",
+        "category": "spell",
+        "href"    : "/spells/X6GQ4ngqpjP8SpCq"
+    },
+    "LIV2gH3ZUFlu0zu4": {
+        "id"      : "LIV2gH3ZUFlu0zu4",
+        "text"    : "Malédiction des vents contraires",
+        "category": "spell",
+        "href"    : "/spells/LIV2gH3ZUFlu0zu4"
+    },
+    "zLYqrQdheciiW2nm": {
+        "id"      : "zLYqrQdheciiW2nm",
+        "text"    : "Malédiction du combattant mortel",
+        "category": "spell",
+        "href"    : "/spells/zLYqrQdheciiW2nm"
+    },
+    "55WR55op9BIdgF1B": {
+        "id"      : "55WR55op9BIdgF1B",
+        "text"    : "Murmures de faiblesse",
+        "category": "spell",
+        "href"    : "/spells/55WR55op9BIdgF1B"
+    },
+    "wYerMk6F1RZb0Fwt": {
+        "id"      : "wYerMk6F1RZb0Fwt",
+        "text"    : "Médecine militaire",
+        "category": "action",
+        "href"    : "/actions/wYerMk6F1RZb0Fwt"
+    },
+    "YNAthsgsJjQIXbc8": {
+        "id"      : "YNAthsgsJjQIXbc8",
+        "text"    : "Négociateur de pacte",
+        "category": "feat",
+        "href"    : "/feats/YNAthsgsJjQIXbc8"
+    },
+    "M8RCbthRhB4bxO9t": {
+        "id"      : "M8RCbthRhB4bxO9t",
+        "text"    : "Ordre de fer",
+        "category": "spell",
+        "href"    : "/spells/M8RCbthRhB4bxO9t"
+    },
+    "LzYi0OuOoypNb6jd": {
+        "id"      : "LzYi0OuOoypNb6jd",
+        "text"    : "Panache",
+        "category": "feat",
+        "href"    : "/feats/LzYi0OuOoypNb6jd"
+    },
+    "6ON8DjFXSMITZleX": {
+        "id"      : "6ON8DjFXSMITZleX",
+        "text"    : "Pas besoin de s'alarmer",
+        "category": "feat",
+        "href"    : "/feats/6ON8DjFXSMITZleX"
+    },
+    "kS4wxSKrZxcOvSNK": {
+        "id"      : "kS4wxSKrZxcOvSNK",
+        "text"    : "Pas du littoral",
+        "category": "action",
+        "href"    : "/actions/kS4wxSKrZxcOvSNK"
+    },
+    "aEM2cttJ2eYcLssW": {
+        "id"      : "aEM2cttJ2eYcLssW",
+        "text"    : "Pas rapide",
+        "category": "action",
+        "href"    : "/actions/aEM2cttJ2eYcLssW"
+    },
+    "QEOe9AhQzdqIx1ei": {
+        "id"      : "QEOe9AhQzdqIx1ei",
+        "text"    : "Pierre stable",
+        "category": "spell",
+        "href"    : "/spells/QEOe9AhQzdqIx1ei"
+    },
+    "yOloZIGkulrrPYG4": {
+        "id"      : "yOloZIGkulrrPYG4",
+        "text"    : "Poumon de feu",
+        "category": "spell",
+        "href"    : "/spells/yOloZIGkulrrPYG4"
+    },
+    "CuVvDrBaVP9nYJlt": {
+        "id"      : "CuVvDrBaVP9nYJlt",
+        "text"    : "Prédire le mal",
+        "category": "spell",
+        "href"    : "/spells/CuVvDrBaVP9nYJlt"
+    },
+    "N91EI7H1p7YXKX0o": {
+        "id"      : "N91EI7H1p7YXKX0o",
+        "text"    : "Puits runique personnel",
+        "category": "feat",
+        "href"    : "/feats/N91EI7H1p7YXKX0o"
+    },
+    "xccOiNL2W1EtfUYl": {
+        "id"      : "xccOiNL2W1EtfUYl",
+        "text"    : "Question imparable",
+        "category": "spell",
+        "href"    : "/spells/xccOiNL2W1EtfUYl"
+    },
+    "plBGdZhqq5JBl1D8": {
+        "id"      : "plBGdZhqq5JBl1D8",
+        "text"    : "Recueillir des informations",
+        "category": "action",
+        "href"    : "/actions/plBGdZhqq5JBl1D8"
+    },
+    "AmV13b9ncALtWJFt": {
+        "id"      : "AmV13b9ncALtWJFt",
+        "text"    : "Rediriger les éléments",
+        "category": "spell",
+        "href"    : "/spells/AmV13b9ncALtWJFt"
+    },
+    "q1iP3SjAF5uceI0M": {
+        "id"      : "q1iP3SjAF5uceI0M",
+        "text"    : "Représentation martiale",
+        "category": "action",
+        "href"    : "/actions/q1iP3SjAF5uceI0M"
+    },
+    "6sNjsNPipZvQ3BGe": {
+        "id"      : "6sNjsNPipZvQ3BGe",
+        "text"    : "Retraite rapide",
+        "category": "action",
+        "href"    : "/actions/6sNjsNPipZvQ3BGe"
+    },
+    "gU7epgcPSm0TD1UK": {
+        "id"      : "gU7epgcPSm0TD1UK",
+        "text"    : "Robes défensives",
+        "category": "feat",
+        "href"    : "/feats/gU7epgcPSm0TD1UK"
+    },
+    "XttSGDuAsRDTuvgS": {
+        "id"      : "XttSGDuAsRDTuvgS",
+        "text"    : "Répercussions de fer",
+        "category": "spell",
+        "href"    : "/spells/XttSGDuAsRDTuvgS"
+    },
+    "Q7QQ91vQtyi1Ux36": {
+        "id"      : "Q7QQ91vQtyi1Ux36",
+        "text"    : "Saut",
+        "category": "action",
+        "href"    : "/actions/Q7QQ91vQtyi1Ux36"
+    },
+    "gxwmzxUWBOhdgFSN": {
+        "id"      : "gxwmzxUWBOhdgFSN",
+        "text"    : "Shélyn",
+        "category": "deity",
+        "href"    : "/deities/gxwmzxUWBOhdgFSN"
+    },
+    "M89l9FOnjHe63wD7": {
+        "id"      : "M89l9FOnjHe63wD7",
+        "text"    : "Thèse arcanique",
+        "category": "feat",
+        "href"    : "/feats/M89l9FOnjHe63wD7"
+    },
+    "UtINSoEJ0u630ia4": {
+        "id"      : "UtINSoEJ0u630ia4",
+        "text"    : "Tirer hors de danger",
+        "category": "action",
+        "href"    : "/actions/UtINSoEJ0u630ia4"
+    },
+    "mxO7u59ms58q7zyj": {
+        "id"      : "mxO7u59ms58q7zyj",
+        "text"    : "Trompeur magique",
+        "category": "spell",
+        "href"    : "/spells/mxO7u59ms58q7zyj"
+    },
+    "r5Uth6yvCoE4tr9z": {
+        "id"      : "r5Uth6yvCoE4tr9z",
+        "text"    : "Vengeance destructrice",
+        "category": "spell",
+        "href"    : "/spells/r5Uth6yvCoE4tr9z"
+    },
+    "1HfusQ8NDWutGvMx": {
+        "id"      : "1HfusQ8NDWutGvMx",
+        "text"    : "Vision animale",
+        "category": "feat",
+        "href"    : "/feats/1HfusQ8NDWutGvMx"
+    },
+    "DUOb8QrlC5t0adkL": {
+        "id"      : "DUOb8QrlC5t0adkL",
+        "text"    : "Vitesse de voyage",
+        "category": "feat",
+        "href"    : "/feats/DUOb8QrlC5t0adkL"
+    },
+    "RDXXE7wMrSPCLv5k": {
+        "id"      : "RDXXE7wMrSPCLv5k",
+        "text"    : "Voler",
+        "category": "action",
+        "href"    : "/actions/RDXXE7wMrSPCLv5k"
+    },
+    "PVXqMko4yGgw90uo": {
+        "id"      : "PVXqMko4yGgw90uo",
+        "text"    : "désir du cœur",
+        "category": "spell",
+        "href"    : "/spells/PVXqMko4yGgw90uo"
+    },
+    "PBQh0amk1FlomV0r": {
+        "id"      : "PBQh0amk1FlomV0r",
+        "text"    : "fioles polyvalentes",
+        "category": "item",
+        "href"    : "/equipment/PBQh0amk1FlomV0r"
+    },
+    "ITWUi1r8Z7EtChkB": {
+        "id"      : "ITWUi1r8Z7EtChkB",
+        "text"    : "Éclair de grandeur",
+        "category": "spell",
+        "href"    : "/spells/ITWUi1r8Z7EtChkB"
+    }
+};
